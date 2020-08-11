d032a044eab689ed609924ed36e310ded82d4110
Nicole Apsche <napsche@kgtiger.com>
2020-08-11 17:29:05 -0400

    reorganizing html layout

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -42,7 +42,7 @@ function losses() {
     reset2();
 }
 $("#crystals1").click(() => {
-    score = score + crystals1;
+    score = score + randomCrystals1;
     $("#score").text(score);
     if (score === randomNumber) {
         win();
@@ -56,7 +56,7 @@ $("#crystals1").click(() => {
     }
 });
 $("#crystals2").click(() => {
-    score = score + crystals2;
+    score = score + randomCrystals2;
     $("#score").text(score);
     if (score === randomNumber) {
         win();
@@ -70,7 +70,7 @@ $("#crystals2").click(() => {
     }
 });
 $("#crystals3").click(() => {
-    score = score + crystals3;
+    score = score + randomCrystals3;
     $("#score").text(score);
     if (score === randomNumber) {
         win();
@@ -83,7 +83,7 @@ $("#crystals3").click(() => {
     }
 });
 $("#crystals4").click(() => {
-    score = score + crystals4;
+    score = score + randomCrystals4;
     $("#score").text(score);
     if (score === randomNumber) {
         win();
@@ -104,8 +104,74 @@ function reset() {
 function reset2() {
     randomNumber = Math.floor(Math.random() * (101 + 1) +19);
     $("#random-number").html(randomNumber);
-    crystals1 = Math.floor(Math.random() * 12) + 1;
-    crystals2 = Math.floor(Math.random() * 12) + 1;
-    crystals3 = Math.floor(Math.random() * 12) + 1;
-    crystals4 = Math.floor(Math.random() * 12) + 1;
+    randomCrystals1 = Math.floor(Math.random() * 12) + 1;
+    randomCrystals2 = Math.floor(Math.random() * 12) + 1;
+    randomCrystals3 = Math.floor(Math.random() * 12) + 1;
+    randomCrystals4 = Math.floor(Math.random() * 12) + 1;
 }
+
+
+// //define variables for game
+// var winCount = 0;
+// var lossCount = 0;
+// var guessRemain = 10; 
+// var guessedLetters = [];
+
+// //set up array for computer choices
+// var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// //have computer pick random letter
+// var randomLetter = Math.floor(Math.random() * alphabet.length);
+// var computerChoice = alphabet[randomLetter];
+// console.log(computerChoice)
+
+// //make event for when key is pressed
+// document.onkeyup = function(event) {
+//     var userChoice = event.key; 
+//     //userChoice must be letter A-Z
+//     var input = /[a-z]/gi; 
+//         if (!input.test(userChoice)) {
+//             alert("Please choose a letter!");
+//         }
+//         else {
+//             console.log(userChoice);
+//         }
+
+//     //computer will choose new letter if user loses
+//     if (guessRemain <= 0) {
+//         lossCount++;
+//         document.getElementById("lossCount").innerHTML = lossCount++;
+//         console.log("You lost!");
+//         alert("You lost!");
+//         guessRemain = 10;
+//         guessedLetters = [];
+//         document.getElementById("guessedLetters").innerHTML = guessedLetters;
+//         document.getElementById("guessRemain").innerHTML = 10;
+//         randomLetter = Math.floor(Math.random() * alphabet.length);
+//         computerChoice = alphabet[randomLetter];
+//         console.log(computerChoice);
+//     }
+
+//     //compare computer and user choice
+//     else if (computerChoice === userChoice) {
+//         winCount++;
+//         console.log("You win!");
+//         alert("You win!");
+//         document.getElementById("winCount").innerHTML = winCount++;
+//         guessedLetters = [];
+//         document.getElementById("guessedLetters").innerHTML = guessedLetters;
+//         randomLetter = Math.floor(Math.random() * alphabet.length);
+//         computerChoice = alphabet[randomLetter];
+//         console.log(computerChoice);
+//         guessRemain = 10; 
+//         document.getElementById("guessRemain").innerHTML = 10;
+//     }
+//     else {
+//         console.log("Guess again!");
+//         document.getElementById("guessRemain").innerHTML = guessRemain--;
+//         guessedLetters.push(userChoice);
+//         document.getElementById("guessedLetters").innerHTML = guessedLetters;
+//     }
+
+    
+// }
